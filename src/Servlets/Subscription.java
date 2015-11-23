945e5b186c42fe483c2455dd121df10e9b6c1176
Maxim Eremin <eremin.max@gmail.com>
2015-11-23 21:11:36 +0300

    I got around with character encoding. Now I know how to change request and response encoding using httpservletreq/resp methods

--- src/Servlets/Subscription.java
+++ src/Servlets/Subscription.java
@@ -8,9 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.PrintWriter;
+import java.sql.*;
 
 /**
  * Created by Max Eremin on 22.11.2015.
@@ -18,7 +17,7 @@ import java.sql.SQLException;
 @WebServlet(name = "Subscription", urlPatterns = {"/subscription"},
     initParams = {
             @WebInitParam(name = "driver", value = "com.mysql.jdbc.Driver"),
-            @WebInitParam(name = "url", value = "jdbc:mysql://localhost:3306/subscribers"),
+            @WebInitParam(name = "url", value = "jdbc:mysql://localhost:3306/subscriberdb"),
             @WebInitParam(name = "username", value = "root"),
             @WebInitParam(name = "password", value = "root")
     }
@@ -26,6 +25,8 @@ import java.sql.SQLException;
 public class Subscription extends HttpServlet {
     Connection connection;
     ServletConfig config;
+    PreparedStatement statement;
+    PrintWriter out;
 
     public void init() throws ServletException{
         config = getServletConfig();
@@ -46,13 +47,64 @@ public class Subscription extends HttpServlet {
     public void destroy() {
         try {
             connection.close();
+            out.close();
         } catch (SQLException e) {
             // close it anyway
         }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Read parameters from request
+        request.setCharacterEncoding("UTF-8");
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String course = request.getParameter("course");
 
+        // Set html page preferences
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        out = response.getWriter();
+
+        // Check if all fields are filled
+        if (name == null || email == null || course == null) {
+            out.println(
+                "<head>\n" +
+                "<title>Ошибка</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<h1>Проверьте, все ли поля заполнены!</h1>\n" +
+                "</body>"
+            );
+        }
+
+        // Add client's data into DB
+        try {
+            statement = connection.prepareStatement("INSERT INTO subscribers(name, email, course) values (?, ?, ?)");
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, course);
+        } catch (SQLException e) {
+            out.println("SQL error in statement found: " + e.getMessage());
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (Exception e) {
+                out.println("Statement error found: " + e.getMessage());
+            }
+        }
+
+        // Create html page
+        out.println(
+            "<head>\n" +
+            "<title>Успешно</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<h1>Добро пожаловать, " + name +
+            "</h1>\nВы успешно подписались на: \"" + course +
+            "\"\n</body>"
+        );
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
